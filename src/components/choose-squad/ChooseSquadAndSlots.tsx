eba799ffed9dc161a15cb431d17e930e4d7e7609
Ben North <ben@redfrontdoor.org>
2024-01-12 20:13:26 +0000

    Require enough players in squad

--- src/components/choose-squad/ChooseSquadAndSlots.tsx
+++ src/components/choose-squad/ChooseSquadAndSlots.tsx
@@ -3,14 +3,24 @@ import { ChooseParams } from "./ChooseParams";
 import { ChooseSquad } from "./ChooseSquad";
 import { useStoreActions, useStoreState } from "../../store";
 import { ignoreValue } from "../../utils";
+import { nPlayersPerCourt } from "../../model/scheduling";
 
 function MakeScheduleButton() {
+  const paramsState = useStoreState((s) => s.scheduleParamsState);
   const makeSchedule = useStoreActions((a) => a.generateSchedule);
   const nSquadMembers = useStoreState((s) => s.squad.size);
 
+  const canMake =
+    paramsState.kind === "loaded" &&
+    nSquadMembers >= paramsState.value.nCourts * nPlayersPerCourt;
+
   return (
     <div className="MakeScheduleButton-container">
-      <Button variant="success" onClick={ignoreValue(makeSchedule)}>
+      <Button
+        disabled={!canMake}
+        variant="success"
+        onClick={ignoreValue(makeSchedule)}
+      >
         Make schedule with {nSquadMembers} people
       </Button>
     </div>
